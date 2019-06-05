19c906fc8f65976c148a542081b666fcfeda1870
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-06-04 21:55:22 -0400

    Bugfix: Add Bean annotation

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -3,10 +3,12 @@ package org.superbiz.moviefun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+    @Bean
     public ServletRegistrationBean getRegistrationBean(ActionServlet servlet) {
         return new ServletRegistrationBean(servlet, "/moviefun/*");
     }
