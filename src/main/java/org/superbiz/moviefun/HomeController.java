27b34e8773f2b15b355bfa05a08ca0260d21bc91
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-06-04 21:36:24 -0400

    Enable transaction support for setup

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
@@ -19,6 +20,7 @@ public class HomeController {
     }
 
     @GetMapping("/setup")
+    @Transactional
     public String setup(Map<String, Object> model) {
         moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
         moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
