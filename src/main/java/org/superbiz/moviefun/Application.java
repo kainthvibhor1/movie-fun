9faf5aa3801d6a1ea9dae22f0cc0fd126fb17a0a
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-06-04 21:53:04 -0400

    Register moviesfun URL with Spring service

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -2,9 +2,14 @@ package org.superbiz.moviefun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 @SpringBootApplication
 public class Application {
+
+    public ServletRegistrationBean getRegistrationBean(ActionServlet servlet) {
+        return new ServletRegistrationBean(servlet, "/moviefun/*");
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
