eb983daa6cc671487f8cd60b51d62f043799b7ce
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-06-06 11:13:52 -0400

    Add background task scheduling using Database

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -3,9 +3,12 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -17,22 +20,35 @@ public class AlbumsUpdateScheduler {
 
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private JdbcTemplate template;
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource source) {
         this.albumsUpdater = albumsUpdater;
+        this.template = new JdbcTemplate(source);
     }
 
 
     @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
     public void run() {
         try {
-            logger.debug("Starting albums update");
-            albumsUpdater.update();
+            if (this.startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+                albumsUpdater.update();
 
-            logger.debug("Finished albums update");
+                logger.debug("Finished albums update");
+            } else {
+                logger.debug("Nothing to start");
+            }
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private boolean startAlbumSchedulerTask() {
+        // Run a SQL Command
+        int rowsUpdated = this.template.update("UPDATE album_scheduler_task SET " +
+                "started_at=NOW() WHERE started_at IS NULL OR TIMESTAMPDIFF(MINUTE, started_at, NOW()) > 2");
+        return rowsUpdated > 0;
     }
 }
